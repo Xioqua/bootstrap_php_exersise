9e81c8a2b2f1dbba1d3684f5e902692cfe133b96
xioqua <1350212416@qq.com>
2018-01-12 02:50:58 +0800

    debug db

--- content.php
+++ content.php
@@ -1,8 +1,13 @@
+<?php
+//session_start();
+include "php/common.php";
+$con = new mysqli('localhost', 'root', 'root', 'myitem');
+if ($con->connect_error) {
+    die('连接失败');
+}
+?>
 <!DOCTYPE html>
 <html lang="zh-CN">
-<?php
-echo $_GET['id'];
-?>
 <head>
     <?php include "include/head.php"; ?>
     <title>内容页</title>
@@ -10,37 +15,45 @@ echo $_GET['id'];
 
 <body>
 <?php include "include/navbar.php"; ?>
-    <div class="container">
-        <?php include "include/carousel.html"; ?>
-        <ol class="breadcrumb">
-            <li><a href="#">首页</a></li>
-            <li><a href="#">前端资讯</a></li>
-            <li class="active">Bootstrap相关优质项目推荐</li>
-        </ol>
-        <div class="text-center page-header">
-            <h3>Bootstrap相关优质项目推荐</h3>
-            <p>
-                作者: <span class="author bg-info text-primary">admin</span>&nbsp;&nbsp; 发布时间: <span class="time bg-info text-primary">2017-05-22 10:52:51</span>
-            </p>
-        </div>
-        <div class="para">
-            <p>就业是中公教育IT培训品牌，致力于培养面向互联网领域的高端人才，以学员就业为目的，优质就业为宗旨，是一家集互联网营销师、UI交互设计师、Web前端工程师、Java工程师、PHP工程师、SEO优化师、SEM竞价师、社会化媒体运营师、电商运营师、互联网产品经理、Android工程师、iOS工程师、C/C++工程师、软件测试工程师、Linux云计算工程师、Python工程师、大数据工程师等课程为一体的IT培训机构。</p>
-            <p>优就业为学员量身打造的个性化、差异化就业流程，力保每一位学员不仅能就业，更能优就业。优就业会根据学员意愿建档统计就职意向,通过理论+实战+实训的学习，通过毕业考核后，将会接受专业的一对一就业服务指导。优就业了解合作企业的岗位需求，可针对各岗位进行差异化培训，打造符合岗位规范的应聘者。同时，会定期举办企业上门招聘双选会，学员可以与心仪的企业面对面沟通，并得到优就业的大力推荐。做到真正对学员负责，对合作企业负责，达到人岗匹配的优化及双方利益的扩大化。</p>
-            <p>优就业总部位于北京，直营分部覆盖国内众多城市，在全国31个省市自治区，近360个地市，拥有500家直营分部和旗舰学习中心，全国专用教学场地超过30万平米。是当前国内直营分部覆盖城市广泛、专职教师数量众多的现代化职业教育机构。</p>
-            <p>优就业业务类型主要包括面授培训、网络远程教学培训、网络直播课程培训、IT类图书出版等。优就业为广大学员提供更加人性化的选课服务，包括就业封闭集训营、周末基础班/提升班、实战实训班次，以及全栈高端班次、在线直播课、录播网课等系列产品。更联合高校开设学历班，学成后将获得国家承认的大专、本科学历，满足广大学员对更多技能、更高学历的追求。</p>
-            <p>优就业拥有一支业内人数众多、阵容强大的培训团队，强大的师资队伍现已拥有逾500名专职培训教师，已率先实现了IT行业师资团队的精细化、科学化分工。创立O2O教学体系，针对全国IT行业需求、发展及就业趋势，打造从线上到线下的全方位、一体化教学体系，目前该体系已帮助众多IT行业学子实现高薪就业梦想。</p>
-        </div>
-        <div class="panel panel-info">
-            <div class="panel-heading">相关文章</div>
-            <ul class="list-group">
-                <li class="list-group-item"><a href="#" class="text-inherit"><span class="glyphicon glyphicon-star-empty"></span> 2016年1月28日荣获： 大众点评2015"十佳职业技术培训品牌奖"</a></li>
-                <li class="list-group-item"><a href="#" class="text-inherit"><span class="glyphicon glyphicon-star-empty"></span> 2016年1月26日荣获: 新京报2015"年度我信赖的培训品牌奖"</a></li>
-                <li class="list-group-item"><a href="#" class="text-inherit"><span class="glyphicon glyphicon-star-empty"></span> 2015年12月12日荣获: 中国网2015"广受学员好评职业教育机构奖"</a></li>
-                <li class="list-group-item"><a href="#" class="text-inherit"><span class="glyphicon glyphicon-star-empty"></span> 2015年12月1日荣获: 腾讯2015"年度实力教育品牌奖"</a></li>
-            </ul>
-        </div>
-        <?php include "include/footer.html"; ?>
+<div class="container">
+    <?php include "include/carousel.html"; ?>
+
+    <ol class="breadcrumb">
+        <li><a href="#">首页</a></li>
+        <li><a href="#">前端资讯</a></li>
+        <li class="active">Bootstrap相关优质项目推荐</li>
+    </ol>
+    <?php
+    $sql = "SELECT * FROM u_article WHERE  u_id=2";
+    $result = $con->query($sql);
+    $row = $result->fetch_assoc();
+    echo '<pre>';
+    var_dump($row);
+    ?>
+    <div class="text-center page-header">
+        <h3>Bootstrap相关优质项目推荐</h3>
+        <p>
+            作者: <span class="author bg-info text-primary">admin</span>&nbsp;&nbsp; 发布时间: <span class="time bg-info text-primary">2017-05-22 10:52:51</span>
+        </p>
     </div>
+    <div class="para">
+        <p>就业是中公教育IT培训品牌，致力于培养面向互联网领域的高端人才，以学员就业为目的，优质就业为宗旨，是一家集互联网营销师、UI交互设计师、Web前端工程师、Java工程师、PHP工程师、SEO优化师、SEM竞价师、社会化媒体运营师、电商运营师、互联网产品经理、Android工程师、iOS工程师、C/C++工程师、软件测试工程师、Linux云计算工程师、Python工程师、大数据工程师等课程为一体的IT培训机构。</p>
+        <p>优就业为学员量身打造的个性化、差异化就业流程，力保每一位学员不仅能就业，更能优就业。优就业会根据学员意愿建档统计就职意向,通过理论+实战+实训的学习，通过毕业考核后，将会接受专业的一对一就业服务指导。优就业了解合作企业的岗位需求，可针对各岗位进行差异化培训，打造符合岗位规范的应聘者。同时，会定期举办企业上门招聘双选会，学员可以与心仪的企业面对面沟通，并得到优就业的大力推荐。做到真正对学员负责，对合作企业负责，达到人岗匹配的优化及双方利益的扩大化。</p>
+        <p>优就业总部位于北京，直营分部覆盖国内众多城市，在全国31个省市自治区，近360个地市，拥有500家直营分部和旗舰学习中心，全国专用教学场地超过30万平米。是当前国内直营分部覆盖城市广泛、专职教师数量众多的现代化职业教育机构。</p>
+        <p>优就业业务类型主要包括面授培训、网络远程教学培训、网络直播课程培训、IT类图书出版等。优就业为广大学员提供更加人性化的选课服务，包括就业封闭集训营、周末基础班/提升班、实战实训班次，以及全栈高端班次、在线直播课、录播网课等系列产品。更联合高校开设学历班，学成后将获得国家承认的大专、本科学历，满足广大学员对更多技能、更高学历的追求。</p>
+        <p>优就业拥有一支业内人数众多、阵容强大的培训团队，强大的师资队伍现已拥有逾500名专职培训教师，已率先实现了IT行业师资团队的精细化、科学化分工。创立O2O教学体系，针对全国IT行业需求、发展及就业趋势，打造从线上到线下的全方位、一体化教学体系，目前该体系已帮助众多IT行业学子实现高薪就业梦想。</p>
+    </div>
+    <div class="panel panel-info">
+        <div class="panel-heading">相关文章</div>
+        <ul class="list-group">
+            <li class="list-group-item"><a href="#" class="text-inherit"><span class="glyphicon glyphicon-star-empty"></span> 2016年1月28日荣获： 大众点评2015"十佳职业技术培训品牌奖"</a></li>
+            <li class="list-group-item"><a href="#" class="text-inherit"><span class="glyphicon glyphicon-star-empty"></span> 2016年1月26日荣获: 新京报2015"年度我信赖的培训品牌奖"</a></li>
+            <li class="list-group-item"><a href="#" class="text-inherit"><span class="glyphicon glyphicon-star-empty"></span> 2015年12月12日荣获: 中国网2015"广受学员好评职业教育机构奖"</a></li>
+            <li class="list-group-item"><a href="#" class="text-inherit"><span class="glyphicon glyphicon-star-empty"></span> 2015年12月1日荣获: 腾讯2015"年度实力教育品牌奖"</a></li>
+        </ul>
+    </div>
+    <?php include "include/footer.html"; ?>
+</div>
 <?php include "include/foot_script.html"; ?>
 </body>
 
