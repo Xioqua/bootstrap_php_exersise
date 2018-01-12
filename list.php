95ee1d86c840a70e1585563c2eed560bb9e3bdc2
xioqua <1350212416@qq.com>
2018-01-12 14:14:10 +0800

    paging using limit

--- list.php
+++ list.php
@@ -5,20 +5,52 @@ $con = new mysqli('localhost', 'root', 'root', 'myitem');
 if ($con->connect_error) {
     die('连接失败');
 }
+/* 如何做分页
+每页条数: 固定
+一共多少页 = 总体数/每页条数
+总条数?
+
+1. 怎么知道当前页
+
+ */
+
 // 处理get参数
 if (empty($_GET['column'])) {
     $column = '全部内容';
 } else {
     $column = $_GET['column'];
 }
+
+$pageSize = 6;  //每页条数
+/* 
+第几页  开始   取几条
+    1:   0     6     
+    2：  6     6
+    3    12    6
+    4   18    6
+公式 N:  (N-1)*6 6  
+N表示当前是第几页,即后来的$pages变量
+*/
+if (empty($_GET['page'])) {
+    // 默认应该从第一页开始
+    $pages = 1;
+} else {
+    $pages = $_GET['page'];
+}
+
+$pageStart = ($pages - 1) * $pageSize;
+
+
 // 不同的column对应不同的sql语句
 if ($column == '全部内容') {
+    // limit [start,] offset  其中下标从0开始
+    $sql = "SELECT * FROM u_article limit $pageStart,$pageSize";
     $sqlTotal = "SELECT * FROM u_article";
+
 } else {
     $sqlTotal = "SELECT * FROM u_article WHERE u_column='$column'";
 }
-$result = $con->query($sqlTotal);
-
+$result = $con->query($sql);
 ?>
 <!DOCTYPE html>
 <html lang="zh-CN">
