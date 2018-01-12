ceba39fff24c94c4644b7a1df8c09ef88dd4ce87
xioqua <1350212416@qq.com>
2018-01-12 19:44:26 +0800

    get pageNum,pageTotal

--- list.php
+++ list.php
@@ -1,89 +1,92 @@
-<?php
-    include "php/common.php";
-    $con = new mysqli('localhost', 'root', 'root', 'myitem');
-    if ($con->connect_error) { die('连接失败'); }
-    if (empty($_GET['column'])) {
-        $column = '全部内容';
-    } else {
-        $column = $_GET['column'];
-    }
-    if (empty($_GET['page'])) {
-        // 默认应该从第一页开始
-        $pages = 1;
-    } else {
-        $pages = $_GET['page'];
-    }
-    $pageSize = 6;
-    $pageStart = ($pages - 1) * $pageSize;
-    if ($column == '全部内容') {
-        $sql = "SELECT * FROM u_article";
-    } else {
-        $sql = "SELECT * FROM u_article WHERE u_column='$column'";
-    }
-    $result = $con->query($sql);
-?>
 <!DOCTYPE html>
+<?php
+include "php/common.php";
+$con = new mysqli('localhost', 'root', 'root', 'myitem');
+if ($con->connect_error) die('连接失败');
+$column = empty($_GET['column']) ? '全部内容' : $_GET['column'];
+$pageSize = 6; //每页显示条数
+$pages = empty($_GET['page']) ? 1 : $_GET['page']; //当前页码
+$pageStart = ($pages - 1) * $pageSize; //从的几条开始
+if( $column == '全部内容' )
+{
+    $sql = "SELECT * FROM u_article ORDER BY u_id LIMIT $pageStart,$pageSize";
+    $sqlTotal = "SELECT * FROM u_article";
+}
+else
+{
+    $sql = "SELECT * FROM u_article WHERE u_column='$column' ORDER BY u_id LIMIT $pageStart,$pageSize";
+    $sqlTotal = "SELECT * FROM u_article WHERE u_column='$column' ORDER BY u_id";
+}
+$result = $con->query($sql);
+$resultTotal = $con->query($sqlTotal);
+echo $resultTotal->num_rows; //得到总条数
+
+$pageNum = ceil($resultTotal->num_rows/$pageSize); //想上去取整得到页码
+echo $pageNum;
+?>
 <html lang="zh-CN">
 <head>
-    <?php include "include/head.php";?>
+    <?php include "include/head.php"; ?>
     <title>列表页</title>
-    <style>.li_line {list-style: none;height: 30px;}</style>
+    <style>.li_line {
+            list-style: none;
+            height: 30px;
+        }</style>
 </head>
 <body>
-    <?php include "include/navbar.php"; ?>
-    <div class="container">
-        <?php include "include/carousel.html"; ?>
-        <ol class="breadcrumb">
-            <li><a href="#">首页</a></li>
-            <li class="active"><?php echo $column;?></li>
-        </ol>
-        <div class="page-header">
-            <h2 class="text-muted"><?php echo $column; ?></h2>
-        </div>
-        <ul class=" list-group list">
-            <?php
-            $j = 0;
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    $j++;
-                    ?>
-                    <li class="list-group-item row">
-                        <a href="#" class="col-md-10"><?php echo $row['u_title'] ?></a>
-                        <span class="text-muted col-md-2"><?php echo date('Y-m-d H:i:s', $row['u_add_date']) ?></span>
-                    </li>
-                    <?php
-                    if ($j % 5 == 0) {
-                        echo '<li class="li_line"></li>';
-                    };
-                }
-            } else {
-                echo '此栏目下没有文章';
-            }
-            ?>
-        </ul>
-
-        <nav aria-label="Page navigation" class="text-center">
-            <ul class="pagination">
-                <li>
-                    <a href="#" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                </li>
-                <li><a href="#">1</a></li>
-                <li><a href="#">2</a></li>
-                <li><a href="#">3</a></li>
-                <li><a href="#">4</a></li>
-                <li><a href="#">5</a></li>
-                <li>
-                    <a href="#" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                </li>
-            </ul>
-        </nav>
-        <?php include "include/footer.html"; ?>
+<?php include "include/navbar.php"; ?>
+<div class="container">
+    <?php include "include/carousel.html"; ?>
+    <ol class="breadcrumb">
+        <li><a href="#">首页</a></li>
+        <li class="active"><?php echo $column; ?></li>
+    </ol>
+    <div class="page-header">
+        <h2 class="text-muted"><?php echo $column; ?></h2>
     </div>
-    <?php include "include/foot_script.html"; ?>
-</body>
+    <ul class=" list-group list">
+        <?php
+        $j = 0;
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $j++;
+                ?>
+                <li class="list-group-item row">
+                    <a href="#" class="col-md-10"><?php echo $row['u_title'] ?></a>
+                    <span class="text-muted col-md-2"><?php echo date('Y-m-d H:i:s', $row['u_add_date']) ?></span>
+                </li>
+                <?php
+                if ($j % 5 == 0) {
+                    echo '<li class="li_line"></li>';
+                }
+            }
+        } else {
+            echo '此栏目下没有文章';
+        }
+        ?>
+    </ul>
 
+    <nav aria-label="Page navigation" class="text-center">
+        <ul class="pagination">
+            <li>
+                <a href="#" aria-label="Previous">
+                    <span aria-hidden="true">&laquo;</span>
+                </a>
+            </li>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li>
+                <a href="#" aria-label="Next">
+                    <span aria-hidden="true">&raquo;</span>
+                </a>
+            </li>
+        </ul>
+    </nav>
+    <?php include "include/footer.html"; ?>
+</div>
+<?php include "include/foot_script.html"; ?>
+</body>
 </html>
