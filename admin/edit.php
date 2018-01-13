9356a10e85354a25f7b1408bbe9bf294f15fad65
xioqua <1350212416@qq.com>
2018-01-13 16:27:23 +0800

    hidden input get id

---
+++ admin/edit.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+include "../php/common.php";
+
+if (empty($_SESSION['htuser'])) {
+    jump('没有登录,请登录', '../admin/admin.php');
+};
+
+$id = $_GET['id'];
+
+$con = new mysqli('localhost', 'root', 'root', 'myitem');
+if ($con->connect_error) {
+    die('连接失败');
+}
+
+$sql = "SELECT * FROM u_article WHERE u_id=$id";
+$result = $con->query($sql);
+$row = $result->fetch_assoc();
+?>
+<!doctype html>
+<html lang="zh-CN">
+<head>
+    <?php include "../include/head.php"; ?>
+    <title>发布文章</title>
+    <style>
+        body {
+            padding-top: 0;
+        }
+
+        .backend .footer {
+            margin-top: 50px;
+        }
+    </style>
+    <link href="umeditor/themes/default/css/umeditor.css" type="text/css" rel="stylesheet">
+    <script type="text/javascript" src="umeditor/third-party/jquery.min.js"></script>
+    <script type="text/javascript" src="umeditor/third-party/template.min.js"></script>
+    <script type="text/javascript" charset="utf-8" src="umeditor/umeditor.config.js"></script>
+    <script type="text/javascript" charset="utf-8" src="umeditor/umeditor.min.js"></script>
+    <script type="text/javascript" src="umeditor/lang/zh-cn/zh-cn.js"></script>
+</head>
+<body class="backend">
+<div class="container">
+    <?php include "include/column_header.php"; ?>
+    <div class="row">
+        <?php include "include/column_nav.html"; ?>
+        <div class="col-md-9" style="border-left:1px solid #eaeaea;">
+            <form method="post" action="php/doedit.php" enctype="multipart/form-data">
+<!--   影藏域传id             t-->
+                <input type="hidden" value="<?php echo $row['u_id']?>" name="id">
+                <div class="form-group">
+                    <label for="exampleInputEmail1">文章标题</label>
+                    <input type="text" class="form-control" name="title" placeholder="文章标题" value="<?php echo $row['u_title'] ?>">
+                </div>
+                <div class="form-group">
+                    <label for="column">栏目名称</label>
+                    <select class="form-control" name="column">
+                        <option>Web前端开发</option>
+                        <option>UI设计</option>
+                        <option>PHP开发</option>
+                        <option>JAVA开发</option>
+                        <option>网络营销</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="describe">文章描述</label>
+                    <!-- 足以读取ytextarea不要空格-->
+                    <textarea class="form-control" rows="3" name="description"><?php echo $row['u_description'] ?></textarea>
+                </div>
+                <div class="form-group">
+                    <label for="keyworld">关键词</label>
+                    <input type="text" class="form-control" id="keyworld" name="keyword" placeholder="关键词" value="<?php echo $row['u_keywords'] ?>">
+                </div>
+                <h5>文章内容</h5>
+                <!-- style给定宽度可以影响编辑器的最终宽度-->
+                <!--style给定宽度可以影响编辑器的最终宽度-->
+                <script type="text/plain" id="myEditor" style="width:100%;height:300px;">
+                    <?php echo $row['u_contents'] ?>
+                </script>
+                <hr>
+                <div class="form-group">
+                    <label for="exampleInputFile">上传缩略图</label>
+                    <input type="file" id="exampleInputFile" name="upfile">
+                    </div>
+                    <input type="submit" class="btn btn-success" value="修改文章">
+                    <input type="reset" class="btn btn-danger" value="重置内容">
+                    </form>
+                    <script type="text/javascript">
+                //实例化编辑器
+                var um = UM.getEditor('myEditor');
+                </script>
+        </div>
+    </div>
+    <?php include '../include/footer.html' ?>
+</div>
+</body>
+</html>
---
+++ admin/php/doedit.php
@@ -0,0 +1,70 @@
+<?php
+include "../../php/common.php";
+session_start();
+$conn = new mysqli('localhost', 'root', 'root', 'myitem');
+if ($conn->connect_error) {
+    die('连接错误');
+};
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $title = $_POST['title'];
+    $column = $_POST['column'];
+    $description = $_POST['description'];
+    $keyword = $_POST['keyword'];
+    $content = $_POST['editorValue'];
+//    $author = $_SESSION['htuser']; //只有登录才能发布文章，记得开启session
+//    $thumb = 'upload/' . $fileNewName;
+//    $times = time();
+
+    $id = $_POST['id'];
+    $file = $_FILES['upfile'];
+
+    if (empty($file['name'])) {
+        $sql = "UPDATE u_article SET u_title='$title',u_column='$column',u_description='$description',u_keywords='$keyword',u_contents='$content' WHERE u_id=$id";
+
+        if ($conn->query($sql)) {
+            jump('修改成功', '../column.php');
+        } else {
+            jump('执行数据库失败', '../edit.php');
+        }
+    }
+
+//       1. 获取错误码
+    if ($file['error'] > 0) {
+        jump('上传错误', '../publish.php');
+    }
+//    2        自定义文件大小
+    $fileSize = $file['size'];
+    $fileMaxSize = 1 * 1024 * 1024; //1m
+    if ($fileSize > $fileMaxSize) {
+        jump('上传文件过大', '../publish.php');
+    }
+//    3 文件类型
+    $fileUploadType = $file['type'];
+    $typeArr = [
+        'image/jpg',
+        'image/png',
+        'image/gif'
+    ];
+
+    if (!in_array($fileUploadType, $typeArr)) {
+        jump('请上传一个图片', '../publish.php');
+    }
+// 4.生成文件名
+    $fileName = $file['name'];
+    $fileNameArr = explode('.', $fileName);
+    $suffixName = array_pop($fileNameArr);
+    $fileNewName = date('YmdHis' . rand(10000, 99999) . '.' . $suffixName);
+//    echo $fileNewName;
+//    5. 移动文件
+    $dir = '../../upload/';
+    if (!move_uploaded_file($file['tmp_name'], $dir . $fileNewName)) {
+        jump('上传失败', '../publish.php');
+    }
+
+} else {
+//        -1
+    jump('非法发布', '../edit.php');
+}
+
